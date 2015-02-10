98aae238e977782441d62578ecc7532e7664f287
Donnie Plumly <donnie.plumly@gmail.com>
2015-02-10 14:30:53 -0800

    Push new modal
    Switched from reveal modal to a custom

---
+++ easterEgg/README.md
@@ -0,0 +1,7 @@
+Duck-Hunt
+=========
+
+Duck Hunt Dog Konami Code
+
+
+This is a Konami code with the perfect score music. In the laughing folder is another option for the Konami code. It has the dog laughing and music. Just sub out the images and music.
--- index.php
+++ index.php
@@ -3,15 +3,12 @@
 $myemail = 'donnie.plumly@gmail.com';
 
 //grab named inputs from html then post to #thanks
-if (isset($_POST['ideaName'])) {
+if (isset($_POST['name'])) {
 $ideaName = strip_tags($_POST['ideaName']);
 $elevatorPitch = strip_tags($_POST['elevatorPitch']);
 $teamList = strip_tags($_POST['teamList']);
 $details = strip_tags($_POST['details']);
-$additional = strip_tags($_POST['additional']);
-$email = strip_tags($_POST['email']);
-$datafile = strip_tags($_POST['datafile']);
-$link = strip_tags($_POST['link']);
+$message = strip_tags($_POST['message']);
 echo "<span class=\"alert alert-success\" >Your message has been received and we will get back to you shortly.</span><br><br>";
 
 // honey pot check
@@ -19,18 +16,19 @@ echo "<span class=\"alert alert-success\" >Your message has been received and we
     //die();
 //} else {
 
-/* GET File Variables  
+/* GET File Variables */ 
 $tmpName = $_FILES['attachment']['tmp_name']; 
 $fileType = $_FILES['attachment']['type']; 
-$fileName = $_FILES['attachment']['name']; */
+$fileName = $_FILES['attachment']['name']; 
  
 /* Start of headers */ 
-$headers = "From: $email"; 
+$headers = "From: $fromName"; 
  
-/*if (file($tmpName)) { 
+if (file($tmpName)) { 
+  /* Reading file ('rb' = read binary)  */
   $file = fopen($tmpName,'rb'); 
   $data = fread($file,filesize($tmpName)); 
-  fclose($file); }*/
+  fclose($file); 
  
   /* a boundary string */
   $randomVal = md5(time()); 
@@ -41,39 +39,36 @@ $headers = "From: $email";
   $headers .= "Content-Type: multipart/mixed;\n" ;
   $headers .= " boundary=\"{$mimeBoundary}\""; 
  
-  /* Multipart Boundary above message 
+  /* Multipart Boundary above message */
   $message = "This is a multi-part message in MIME format.\n\n" . 
   "--{$mimeBoundary}\n" . 
   "Content-Type: text/plain; charset=\"iso-8859-1\"\n" . 
   "Content-Transfer-Encoding: 7bit\n\n" . 
   $message . "\n\n"; 
- */
-  /* Encoding file data 
-  $data = chunk_split(base64_encode($data)); */
  
-  /* Adding attchment-file to message
+  /* Encoding file data */
+  $data = chunk_split(base64_encode($data)); 
+ 
+  /* Adding attchment-file to message*/
   $message .= "--{$mimeBoundary}\n" . 
   "Content-Type: {$fileType};\n" . 
   " name=\"{$fileName}\"\n" . 
   "Content-Transfer-Encoding: base64\n\n" . 
   $data . "\n\n" . 
-  "--{$mimeBoundary}--\n"; */
-
+  "--{$mimeBoundary}--\n"; 
+} 
 
 //generate email and send!
 $to = $myemail;
-$email_subject = "Contact form: $ideaName";
+$email_subject = "Contact form: $name";
 $email_body = "You have received a new message on  \n".
 "Idea Name: $ideaName \n ".
 "Elevator Pitch: $elevatorPitch \n ".
 "Team List: $teamList \n ";
 "Details: $details \n ";
-"Additional: $additional \n ";
-"Email: $email \n ";
-"datafile: $datafile \n ";
-"link: $link \n ";
+"Message: $message \n ";
 $headers = "\n";
-$headers .= "Reply-To: $myemail";
+$headers .= "Reply-To: $email";
 mail($to,$email_subject,$email_body,$headers);
-}
+}}
 ?>
